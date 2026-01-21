cb4c323e47be362d2c1c2841e10d36f8e0aec9a0
TrangLe <alicelee1512@gmail.com>
2026-01-20 19:31:42 -0800

    setup backend initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mood-checkin
 Repository for the mood checking project
+
+Frontend was generated with vite, using React typescript
+Backend was generated with https://start.spring.io/, java springboot
---
+++ backend/src/test/java/com/ada/trang/moodcheckin/MoodcheckinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.trang.moodcheckin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodcheckinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
